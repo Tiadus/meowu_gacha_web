59ad2499e2bc5fe097425443669bac85df649356
Viet Thai Nguyen <vietthai.nguyen03@gmail.com>
2025-08-01 09:59:20 +0000

    feat(site-map) Add site map generator

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://meowu-gacha-web.vercel.app', // Change to your actual domain
+  generateRobotsTxt: true, // (optional)
+  sitemapSize: 5000,
+};
